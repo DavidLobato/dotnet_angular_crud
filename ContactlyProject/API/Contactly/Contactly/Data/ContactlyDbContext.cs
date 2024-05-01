1794da2f49b520e6e45e2102d3f45e259c98a8ab
David Lobato <david.lobato@gmail.com>
2024-05-01 18:59:46 -0300

    Project Contactly built in dotnet and angular together with tailwindcss.

---
+++ ContactlyProject/API/Contactly/Contactly/Data/ContactlyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Contactly._Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contactly.Data
+{
+    public class ContactlyDbContext :DbContext
+    {
+        public ContactlyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
